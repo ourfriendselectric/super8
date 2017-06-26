799c133afb8b5d0990332cc9586f0ad23afe699e
Patrick Wadden <patrickwadden@Patricks-MBP.gateway>
2017-06-26 20:47:34 +1000

    Css updates and font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,20 +31,37 @@
         <section class="details">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-10 col-sm-offset-1">
-                        <p>Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas faucibus mollis interdum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum.</p>
+                    <div class="col-sm-8 col-sm-offset-2">
+                        <h2>What is Super 8</h2>
+                        <p>Super 8 is music competition open to bands, composers, songwriters, bedroom producers - basically anyone who can create an origianl piece of music.</p>
+                        <p>We have recorded a music video for your copmposition to be written and tracked to. Your entry will be judged not only on how good the tune is, however also on how well the song compliments the video and the story within.</p>
+                        <p>The comp culminates with a live awards and performance evening at Sydney's Oxford Art Factory where the top, or 'Super' 8  nalists perform their tracks to a panel of industry heavy weights including artists, producers, sound engineers and label heads.</p>
+                        <p>The comp is split into two categories, 'performance' and 'non performance'. Basically if you need a 24 piece orchestra to perform your track, or have electronically created a bunch of samples featuring a sped up humming bird or the sound of a catepillar crying you  t into the 'non performance' category.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2">
+                        <h2>The Rules</h2>
+                        <p>Your song must be an original composition.<br>
+                        It cannot have been entered in another competition previously or used commercially.<br>
+                        If you have written and recorded a song but not yet released it, this is your perfect opportunity!</p>
+                        <p>This competition is about composing a piece of music to match a video.</p>
+                        <p>Your composition must tell the story of the video. It must also be mixed, mastered &amp; synced to the video.</p>
+                        Super 8 is open to Australian Citizens &amp; Permanent Residence.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2">
+                        <h2>How To Enter</h2>
+                        <p>Register &amp; download a copy of our music video - available at the top of this page.</p>
+                        <p>Once your track is ready, upload a copy in the required file formats, select if your track can or cannot be performed live and you are good to go.</p>
+                        <p>There is a one off entry fee of $50 per song entered.</p>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-10 col-sm-offset-1">
-                        <p>Aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id eli.</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-10 col-sm-offset-1">
-                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                        <p>Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus.</p>
+                        <h2>Judges &amp; Prizes</h2>
+                        <p>Stay tuned. Sign up to our mailing list &amp; follow us on facebook &amp; instagram for announcements.</p>
                     </div>
                 </div>
             </div>
