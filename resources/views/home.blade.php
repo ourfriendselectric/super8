ccda12a3a159c1e92af328e0f0bfabe2a187bab6
Patrick Wadden <patrickwadden@Patricks-MBP-2.gateway>
2017-08-24 21:31:07 +1000

    Typos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,9 +50,9 @@
                             <img src="/images/lightning.png" alt="" class="lightning">
                             <h2>What is Super 8</h2>
                             <p>Super 8 is music competition open to bands, composers, songwriters, bedroom producers - basically anyone who can create an origianl piece of music.</p>
-                            <p>We have recorded a music video for your copmposition to be written and tracked to. Your entry will be judged not only on how good the tune is, however also on how well the song compliments the video and the story within.</p>
-                            <p>The comp culminates with a live awards and performance evening at Sydney's Oxford Art Factory where the top, or 'Super' 8  nalists perform their tracks to a panel of industry heavy weights including artists, producers, sound engineers and label heads.</p>
-                            <p>The comp is split into two categories, 'performance' and 'non performance'. Basically if you need a 24 piece orchestra to perform your track, or have electronically created a bunch of samples featuring a sped up humming bird or the sound of a catepillar crying you  t into the 'non performance' category.</p>
+                            <p>We have recorded a music video for your composition to be written and tracked to. Your entry will be judged not only on how good the tune is, however also on how well the song compliments the video and the story within.</p>
+                            <p>The comp culminates with a live awards and performance evening at Sydney's Oxford Art Factory where the top, or 'Super' 8  finalists perform their tracks to a panel of industry heavy weights including artists, producers, sound engineers and label heads.</p>
+                            <p>The comp is split into two categories, 'performance' and 'non performance'. Basically if you need a 24 piece orchestra to perform your track, or have electronically created a bunch of samples featuring a sped up humming bird or the sound of a catepillar crying you go into the 'non performance' category.</p>
                         </div>
                     </div>
                 </div>
@@ -83,7 +83,7 @@
                             <h2>How To Enter</h2>
                             <p>Register &amp; download a copy of our music video - available at the top of this page.</p>
                             <p>Once your track is ready, upload a copy in the required file formats, select if your track can or cannot be performed live and you are good to go.</p>
-                            <p>There is a one off entry fee of $50 per song entered.</p>
+                            <p>There is a one off entry fee of $55 per song entered.</p>
                         </div>
                     </div>
                 </div>
